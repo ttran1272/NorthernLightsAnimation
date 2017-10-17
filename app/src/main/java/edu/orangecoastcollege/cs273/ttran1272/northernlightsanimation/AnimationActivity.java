a5ccceab1a8639632953642be2b144be4e5bb3b5
ttran1272 <tu_anh11@hotmail.com>
2017-10-17 16:04:13 -0700

    more updates

--- app/src/main/java/edu/orangecoastcollege/cs273/ttran1272/northernlightsanimation/AnimationActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/ttran1272/northernlightsanimation/AnimationActivity.java
@@ -1,13 +1,71 @@
 package edu.orangecoastcollege.cs273.ttran1272.northernlightsanimation;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class AnimationActivity extends AppCompatActivity {
+
+    // AnimationDrawable = used for frame animations
+    private AnimationDrawable frameAnim;
+
+    // Animation = used for tween(ed) animations
+    private Animation rotateAnim;
+    private Animation shakeAnim;
+    private Animation customAnim;
+    private ImageView lightsImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_animation);
+
+        lightsImageView = (ImageView) findViewById(R.id.lightsImageView);
+    }
+
+    public void toggleFrameAnim(View view){
+        // hasn't  been initialized yet
+        if (frameAnim == null)
+        {
+            lightsImageView.setBackgroundResource(R.drawable.frame_anim);
+            frameAnim = (AnimationDrawable) lightsImageView.getBackground();
+        }
+
+        // if frameAnim is running, stop it
+        if (frameAnim.isRunning())
+            frameAnim.stop();
+        else
+            // else start it
+            frameAnim.start();
+
+    }
+
+    public void toggleRotateAnim(View view){
+
+        // hasn't been initialized yet
+        if (rotateAnim == null)
+            rotateAnim = AnimationUtils.loadAnimation(this, R.anim.rotate_anim);
+
+        // if hasn't started or it has ended, then start it
+        if (!rotateAnim.hasStarted() || rotateAnim.hasEnded())
+            lightsImageView.startAnimation(rotateAnim);
+        else
+            // Connect it to the image view
+            lightsImageView.clearAnimation();
+    }
+
+    public void toggleShakeAnim(View view){
+
+        // hasn't been initialized yet
+        shakeAnim = AnimationUtils.loadAnimation(this, R.anim.shake_anim);
+        lightsImageView.startAnimation(shakeAnim);
+    }
+
+    public void toggleCustomAnim(View view){
+
     }
 }
